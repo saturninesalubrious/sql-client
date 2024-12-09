ef3c0f65f42873b6cb1699d9287565bd3bd5eb4e
avinash <avinashsharma17012002@gmail.com>
2024-12-09 08:56:58 +0530

    sql-client pg library

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgresql://my_database_c7vf_user:aXh4bHzZzyre5XbmLO7GHUzXFDsnsbZ1@dpg-ctasjuggph6c73es2rb0-a/my_database_c7vf");
     await client.connect();
     return client;
 }
+
